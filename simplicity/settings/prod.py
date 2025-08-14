12287554042d8973e9b3bda79656494774dd8932
Lars Lover <larsroaldlover@gmail.com>
2025-08-14 21:12:37 +0200

    trying ussing app password

--- simplicity/settings/prod.py
+++ simplicity/settings/prod.py
@@ -22,7 +22,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'larsroaldlover@gmail.com'
-EMAIL_HOST_PASSWORD = '2Cor5:17'
+EMAIL_HOST_PASSWORD = 'whrbrshoxxjctqgh'
 
 
 
