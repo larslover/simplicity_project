d1d419b492a4e54bbb84f73a028a92285ed80995
Lars Lover <larsroaldlover@gmail.com>
2025-08-14 20:59:31 +0200

    changed to gmail

--- simplicity/settings/prod.py
+++ simplicity/settings/prod.py
@@ -18,11 +18,12 @@ STATIC_ROOT = BASE_DIR / 'simplicity' / 'staticfiles'
 # Email using business account
 # Email using business account
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lars@simplicitysapps.com'
-EMAIL_HOST_PASSWORD = '2Cor5:17!'
+EMAIL_HOST_USER = 'larsroaldlover@gmail.com'
+EMAIL_HOST_PASSWORD = '2Cor5:17'
+
 
 
 
