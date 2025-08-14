c46b90b6f97e0fb94f7b1256ce120b888d7abfdf
Lars Lover <larsroaldlover@gmail.com>
2025-08-14 21:53:07 +0200

    codestuff

--- simplicity/settings/prod.py
+++ simplicity/settings/prod.py
@@ -8,37 +8,26 @@ ALLOWED_HOSTS = [
     'www.simplicitysapps.com',
 ]
 
-# Secret key from environment variable
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'replace-this-with-env-secret')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-# Static files
 STATIC_ROOT = BASE_DIR / 'simplicity' / 'staticfiles'
 
-
-# Email using business account
-# Email using business account
-# Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-
-
-
-
-# MySQL database on PythonAnywhere (adjust username and DB)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'yourusername$simplicity'),
-        'USER': os.getenv('DB_USER', 'yourusername'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'yourusername.mysql.pythonanywhere-services.com'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
